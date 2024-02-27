c48e495fb15320a246272514bc35434ac1d5761d
Rizky Ertanto <ertantorizkyf@gmail.com>
2024-02-27 16:59:52 +0700

    Create auth constants

---
+++ src/constants/auth.rs
@@ -0,0 +1,4 @@
+pub const DEFAULT_JWT_SECRET: &str = "jwt_secret";
+pub const DUMMYMAIL: &str = "jane@mail.com";
+pub const DUMMYNAME: &str = "Jane Doe";
+pub const DUMMYPASS: &str = "$2b$12$Fl7nlBAdlBu.RqsHUHhbh.wH8ARaI6aAT4P/AaPkrCNs5zlN7hOKy";
