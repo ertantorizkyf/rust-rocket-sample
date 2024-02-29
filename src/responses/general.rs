02475a8b18bf4a4387c6bc31eaf3b3f043eed23e
Rizky Ertanto <ertantorizkyf@gmail.com>
2024-02-29 10:49:28 +0700

    Enable general response deserilization for unit test

--- src/responses/general.rs
+++ src/responses/general.rs
@@ -1,6 +1,6 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct GeneralResponse {
     pub status: String,
     pub message: String
